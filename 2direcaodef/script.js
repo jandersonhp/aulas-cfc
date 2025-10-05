65d95020f017e8e6aad472eff4184a6742ec38c4
Janderson <jandersonduarte@yahoo.com.br>
2025-10-05 15:53:47 -0300

    estrut

---
+++ 2direcaodef/script.js
@@ -0,0 +1,4 @@
+// Exemplo de interação
+document.getElementById("botaoExemplo").addEventListener("click", function() {
+    document.getElementById("resposta").innerText = "Você clicou no botão! Aqui você pode adicionar interações específicas para o módulo.";
+});
